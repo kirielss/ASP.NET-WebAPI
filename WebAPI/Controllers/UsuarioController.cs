bf01f8ce9ae61513d80f04d3e736bd2a1f5100cb
skiriel <gabrieldllopes@gmail.com>
2023-03-27 22:02:07 -0300

    feat: API Delete & Put

--- WebAPI/Controllers/UsuarioController.cs
+++ WebAPI/Controllers/UsuarioController.cs
@@ -36,5 +36,20 @@ namespace WebAPI.Controllers
             UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
             return Ok(usuario);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            bool apagado = await _usuarioRepositorio.Apagar(id);
+            return Ok(apagado);
+        }
     }
 }
